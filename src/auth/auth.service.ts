004ad9421beacedb96de85e6cbbc90dcf31c3ded
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-25 02:16:03 +0330

    checking unique of username and email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,29 +4,41 @@ import { LoginAuthDto } from './dto/login-auth.dto';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from "bcrypt";
 import { JwtService } from '@nestjs/jwt';
+import passport from 'passport';
 @Injectable()
 export class AuthService {
 
   constructor (
-    private readonly usersService: UserService,
+    private readonly userService: UserService,
     private readonly jwtService: JwtService
   ){}
 
   async register(dto: RegisterAuthDto) {
     const hashed = await bcrypt.hash(dto.password, 10);
-    const user = await this.usersService.create({
+    
+    // Handle unique of username and email
+    // if (await this.userService.findByIdentifier(dto.email))
+    //   throw new UnauthorizedException('Email already exists');
+    
+    // if (await this.userService.findByIdentifier(dto.username))
+    //   throw new UnauthorizedException('Username already exists');
+
+    // Handle unique of username and email
+    const user = await this.userService.create({
       email: dto.email,
       password: hashed,
       fullName: dto.fullName,
       username: dto.username
     });
+    
     return this._generateToken(user);
   }
 
   async login(dto: LoginAuthDto) {
-    const user = await this.usersService.findByEmail(dto.username);
+    const user = await this.userService.findByIdentifier(dto.identifier);
+
     if(!user || !(await bcrypt.compare(dto.password, user.password)))
-      throw new UnauthorizedException('Invalid credintials')
+      throw new UnauthorizedException('Invalid credintials');
 
     return this._generateToken(user);
   } 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,10 +5,9 @@
 
 import { UserDocument, User } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -17,11 +16,31 @@ export class UserService {
     
     async create(dto: CreateUserDto): Promise<User> {
         const user = new this.userModel(dto);
-        return user.save();
+        
+        try {
+            return await user.save();
+        }
+        catch (err) {
+            if(err.code === 11000) {
+                const field = Object.keys(err.keyPattern)[0];
+                throw new ConflictException(`The ${field} already exists.`)
+            }
+            return err;
+        }
     }
 
-    async findByEmail(email: string): Promise<User | null> {
-        return this.userModel.findOne({ email });
+    // This means it finds a user by email or username
+    async findByIdentifier(identifier: string): Promise<User | null> {
+        
+        if (this.isEmail(identifier))
+            return this.userModel.findOne({ email: identifier });
+        else 
+            return this.userModel.findOne({ username: identifier })
+    }
+
+    private isEmail(value: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(value);
     }
 }
 
