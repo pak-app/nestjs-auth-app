af8b51e6c573aacab1e7c4f24f9d24d97312f3b7
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-24 04:14:23 +0330

    create user module and setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`put_mongodb_address`), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
