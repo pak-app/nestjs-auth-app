4b8dd4395f87bf149ee093359ad9e43b1314a933
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-26 01:24:36 +0330

    add validation pipe for auth controller
    This validatino pipe ignores all keys don't exist inside related dto.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get, Post, Body, UseGuards, Request, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Request, ValidationPipe, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
+// You can add this validation globally to remove all forbiden keys
 @Controller('auth')
+@UsePipes(
+  new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  })
+)
 export class AuthController {
   
   constructor(private readonly authService: AuthService) {}
