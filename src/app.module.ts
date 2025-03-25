f4934dc093c5edf4914d7e93440e83fbb82c3b22
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-26 01:06:46 +0330

    fix bug auth failure in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module, Logger, ConsoleLogger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -6,10 +6,14 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 // import configuration from './config/configuration';
+console.log()
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DATABASE_CONTAINER_NAME}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI as string),
   UserModule,
   AuthModule
 ],
