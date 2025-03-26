6a3058a233a2de000994b8e44658430eb1239bd5
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-03-26 05:13:56 +0330

    add correct host address for deployment and debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 // import configuration from './config/configuration';
 console.log()
@@ -13,7 +13,22 @@ console.log()
       isGlobal: true,
       envFilePath: ".env"
     }),
-    MongooseModule.forRoot(process.env.DATABASE_URI as string),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        const username = config.get('MONGO_INITDB_ROOT_USERNAME');
+        const password = config.get('MONGO_INITDB_ROOT_PASSWORD');
+        const host = config.get('DATABASE_HOST'); // 'localhost' for host machine
+        const port = config.get('DATABASE_PORT');
+        const db = config.get('DATABASE_NAME');
+
+        const uri = `mongodb://${username}:${password}@${host}:${port}/${db}?authSource=admin`;
+
+        console.log('[MongoDB URI]', uri); // ✅ Optional debug
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
   UserModule,
   AuthModule
 ],
